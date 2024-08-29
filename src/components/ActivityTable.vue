162fb1f2d8dc3b26e42efcf13a43339ae6bd1f86
Steffen Geving <gevis1@student.op.ac.nz>
2024-08-29 22:49:34 +1200

    feat: use lazy updates on activity table inputs

--- src/components/ActivityTable.vue
+++ src/components/ActivityTable.vue
@@ -1,66 +1,7 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { inject } from 'vue';
 
-import { Activity } from '../assets/activities';
-
-const stringData = window.localStorage.getItem("activityData");
-let storageActivities = null;
-
-function isActivity(data: any): data is Activity {
-    return typeof data.name === "string"
-        && typeof data.pre  === "string"
-        && (typeof data.time === "number" || typeof data.time === "string")
-}
-
-function isActivityArray(data: any): data is Array<Activity> {
-    let result = true;
-
-    for (const d in data) {
-        if (!isActivity(d)) {
-            result = false;
-        }
-    }
-
-    return result;
-}
-
-console.log("stringData", stringData)
-
-if (stringData) {
-    storageActivities = JSON.parse(stringData);
-}
-
-console.log("storageActivities", storageActivities)
-
-interface ActivityTableData {
-    activities: Array<Activity>
-}
-
-//TODO: Update localStorage
-
-let data = reactive<ActivityTableData>({
-    activities: Array(10).fill({
-        name: "\u00A0",
-        pre: "\u00A0",
-        time: "\u00A0"
-    })
-});
-
-console.log("data.activities", data.activities)
-
-// TODO: Test this for robustness
-if (storageActivities !== null && isActivityArray(storageActivities)) {
-    data.activities = storageActivities;
-}
-
-console.log("data.activities", data.activities)
-
-console.log("data.activities[0]", data.activities[0])
-
-console.log("data.activities[0].name", data.activities[0].name)
-console.log("data.activities[0].pre", data.activities[0].pre)
-console.log("data.activities[0].time", data.activities[0].time)
-
+const activityData = inject("activityData");
 </script>
 
 <template>
@@ -70,10 +11,10 @@ console.log("data.activities[0].time", data.activities[0].time)
         <th class="activity prerequisite">Pre-requisites</th>
         <th>Time</th>
     </tr>
-    <tr v-for="activity in data.activities">
-        <td class="activity name"> {{ activity.name ?? '\u00A0' }}</td>
-        <td class="activity prerequisite"> {{ activity.pre ?? '\u00A0' }}</td>
-        <td class="activity time"> {{ activity.time ?? '\u00A0' }}</td>
+    <tr v-for="activity in activityData">
+        <td class="activity name"><input v-model.lazy="activity.name"></input></td>
+        <td class="activity prerequisite"><input v-model.lazy="activity.pre"></td>
+        <td class="activity time"><input v-model.lazy="activity.time"></td>
     </tr>
   </table>
 </template>
